2bda27a60aedba6dd7319f9b47101c34d1b96872
robermancera <robertocarlos_flores@ucol.mx>
2021-09-07 11:43:17 -0500

    Creamos, la primer clase y sus primeros metodos, tambien intentamos hacer las primeras pruebas con rspec pero no hemos podido avanzar

---
+++ classes/shoot.rb
@@ -0,0 +1,24 @@
+# strike tiró todos los pines en un intento 
+# spare tiró los pines pero utilizó los dos intentos
+# Hacer un spare es tirar los diez en dos tiros
+# open o normal que no tiró todos los pines
+# frame es turno, no es tiro
+# miss
+
+class Shoot
+  def self.get_type(frame)
+      type = :strike if self.is_strike?(frame)
+      type = :spare if self.is_spare?(frame)
+      type = :normal if self.is_normal?(frame)
+  end
+  def self.is_strike?(frame)
+      return true if frame[:first_shoot] == 10
+  end
+  def self.is_spare? (frame)
+      return true if (frame[:first_shoot] + frame[:second_shoot] == 10) and (frame[:first_shoot] < 10)
+  end
+  def self.is_normal?
+      return true if frame[:first_shoot] + frame[:second_shoot] < 10
+  end
+end
+
---
+++ test/shoot_rspec.rb
@@ -0,0 +1,21 @@
+require 'rspec'
+require '../classes/shoot.rb'
+
+describe Shoot do
+    describe ".get_type" do
+        it 'Returns strike type ' do
+          strike = {:first_shoot => 10, :two => 0}
+          expect(Shoot.get_type(strike)).to eq :strike
+        end
+
+        it 'Es un spare' do
+          spare = {:first_shoot => 8, :second_shoot => 2}
+          expect(Shoot.get_type(spare)).to eq :spare
+         end
+
+         it 'Es un abierto' do
+          normal = {:first_shoot => 3, :second_shoot => 3}
+          expect(Shoot.get_type(normal)).to eq :strike
+         end
+    end
+end
