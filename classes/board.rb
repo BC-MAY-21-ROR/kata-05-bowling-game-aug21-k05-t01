1bf15aad99a7501ccd359c6a297a2745be029700
josemanuelinc <joseinc@hotmail.es>
2021-09-08 13:22:44 -0500

    Board and BowlingGame classes modified , added calculate methods to board class and printing methods for bowling_game

--- classes/board.rb
+++ classes/board.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
 
-require_relative './shoot.rb'
+require_relative './shoot'
 
+# Class Board
 class Board
   attr_reader :board
 
@@ -9,13 +11,36 @@ class Board
     @total_score = 0
     init_board
   end
-  
+
   def init_board
-    (1..10).each do 
-      @board << { first_shoot: 0, second_shoot: 0, total_turn_score: 0}
+    10.times do
+      @board << { first_shoot: 0, second_shoot: 0, total_turn_score: 0 }
+    end
+  end
+
+  def calculate_strike(index)
+    score = @board[index - 1][:total_turn_score] + 10
+    next_frame = @board[index + 1]
+    total_score = calculate_total_turn_score(next_frame, score, index)
+    @board[index][:total_turn_score] = total_score
+  end
+
+  def calculate_spare(index)
+    next_value = @board[index + 1][:first_shoot] unless @board[index + 1].nil?
+    @board[index][:total_turn_score] = @board[index - 1][:total_turn_score] + 10 + next_value.to_i
+  end
+
+  def calculate_normal(index)
+    frame = @board[index]
+    score = @board[index - 1].nil? ? 0 : @board[index - 1][:total_turn_score]
+    @board[index][:total_turn_score] = score + (frame[:first_shoot] + frame[:second_shoot])
+  end
+
+  def calculate_total_turn_score(frame, index_ctts, given_score)
+    if Shoot.get_type(frame) == :strike
+      given_score + (frame[:first_shoot] + @board[index_ctts + 2][:first_shoot])
+    else
+      given_score + (frame[:first_shoot] + frame[:second_shoot])
     end
   end
 end
-
-board1 = Board.new
-print board1.board
--- classes/bowling_game.rb
+++ classes/bowling_game.rb
@@ -2,21 +2,36 @@
 
 require_relative './board'
 
-#Clase Juego que aplica todos los metodos a las demas clases
+# Clase Juego que aplica todos los metodos a las demas clases
 class BowlingGame
-  def initalize
+  def initialize
     @board_father = Board.new
+    @board_child = @board_father.board
+    display_points
   end
 
   def display_points
-    print '>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>'
+    clear_display
+    print "\n"
+    print 'PUNTAJES'.center(58, '=========================')
+    print "\n"
+    @board_child.each_with_index { |frame, index| print_formated(index, frame) }
+    print 'PUNTAJE TOTAL'.center(58, '=========================')
+  end
+
+  def print_formated(index, frame)
+    if index + 1 < 10
+      puts "|| Frame number #{index + 1}  =>  shoot one: #{frame[:first_shoot]}" \
+        " shoot two #{frame[:second_shoot]} total: #{frame[:total_turn_score]} || "
+    elsif index + 1 == 10
+      puts "|| Frame number #{index + 1} =>  shoot one: #{frame[:first_shoot]}" \
+      " shoot two #{frame[:second_shoot]} total: #{frame[:total_turn_score]} || "
+    end
   end
 
   def clear_display
-    if RUBY_PLATFORM =~ /win32|win64|\.NET|windows|cygwin|mingw32/i
-      system('cls')
-    else
-      system('clear')
-    end
+    Gem.win_platform? ? (system 'cls') : (system 'clear')
   end
 end
+
+BowlingGame.new
